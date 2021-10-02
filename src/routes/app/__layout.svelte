07ee772b0f4da4dc46e561834b283db41bc1fe25
gyurielf <beyourself223@gmail.com>
2021-10-02 13:19:24 +0200

    feat: initial commit

---
+++ src/routes/app/__layout.svelte
@@ -0,0 +1,21 @@
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+	import { browser } from '$app/env';
+
+	export const load: Load = async ({ page, fetch, session, stuff }) => {
+		if (browser) {
+			return {};
+		}
+	};
+</script>
+
+<script lang="ts">
+</script>
+
+<div class="container">
+	<slot />
+</div>
+
+<style lang="scss" global>
+	// @import 'src/lib/styles/main';
+</style>
